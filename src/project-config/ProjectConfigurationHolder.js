b8e475eb5dcf29ef786e9b6838ed6c5ff4112c30
c3s4r <capadron@miners.utep.edu>
2022-09-20 13:59:20 -0600

    Project config fixed

--- src/project-config/ProjectConfigurationHolder.js
+++ src/project-config/ProjectConfigurationHolder.js
@@ -35,34 +35,40 @@ class ProjectConfigurationHolder extends Component {
         return (
             
             <div className='config'>
+                 <div className= 'topButton'>
+                    <button type='Min'>_</button>
+                    <button type='Max'>O</button>
+                    <button type='exit'>X</button>   
+                </div>
+                <div className='configTitle'>
+                    <label >Create New Project</label>
+                </div>
+               
                 {/* <div className='container'> */}
                 <form onSubmit={this.handleSubmit}>
+                    
+                    
                     <div>
-                        <label className='configCreateLocation'>Create New Project</label>
+                        <label className='configLabel'>Project Name</label>
+                        <label className='configLabel'style={{color:'red'}}>Required</label>
+                        <input className='configInput' type="text" value={projectName} onChange={this.handleProjectName} />
                     </div>
                     <div>
-                        <label className='configLabelProjectname'>Project Name</label>
-                        <input className='configInputProjectname' type="text" value={projectName} onChange={this.handleProjectName} />
-                        <label className='configLabelRequired'>Required</label>
-                    </div>
-                    <div>
-                        <label className='configLabelLocation'>Stored Location</label>
-                        <input className='configInputLocation' type="text" value={storedLocation} onChange={this.handleStoredLocation} />
-                        <label className='configSecLabelRequired'>Required</label>
+                        <label className='configLabel'>Stored Location</label>
+                        <label className='configLabel'style={{color:'red'}}>Required</label>
+                        <div className='buttonInput'>
+                            <input className='input' type="text" value={storedLocation} onChange={this.handleStoredLocation} />  
+                            <button className= 'browseButton'type='Browse'>Browse</button>
+                        </div>
+                        
+                        
                     </div>
                     <br />
                     <div>
-                    <button className= 'continueButton'type='Continue'>Continue</button>
-                    <button className= 'cancelButton'type='Cancel'>Cancel</button>
+                    <button className= 'bottomButton'type='Continue'>Continue</button>
+                    <button className= 'bottomButton'type='Cancel'>Cancel</button>
                     </div>
-                    <div>
-                    <button className= 'browseButton'type='Browse'>Browse</button>
-                    </div>
-                    <div>
-                    <button className= 'minButton'type='Min'>_</button>
-                    <button className= 'maxButton'type='Max'>O</button>
-                    <button className= 'exitButton'type='exit'>X</button>   
-                    </div>
+                    
                 </form>
                 {/* </div>     */}
             </div>
