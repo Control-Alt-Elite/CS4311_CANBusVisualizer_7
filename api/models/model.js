6fd3771faafdeaeb7223e7676449e52ee4d4ab2b
bamariscal <78370129+bamariscal@users.noreply.github.com>
2022-09-28 10:09:00 -0600

    merged the files from FrontBackMerge

--- api/models/model.js
+++ api/models/model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const mySchema = new Schema({
+const projectSchema = new Schema({
     projectName: {
         type: String,
         required: false
@@ -11,7 +11,39 @@ const mySchema = new Schema({
 		required: false
 	},
 });
+const sessionSchema = new Schema({
+    analystInitials: {
+        type: String,
+        required: false
+    },
+    projectDate: {
+		type: String,
+		required: false
+	},
+    canConnectorID: {
+		type: String,
+		required: false
+	},
+    vehicleID: {
+		type: String,
+		required: false
+	},
+    baudRate: {
+		type: String,
+		required: false
+	},
+    blacklistFileName: {
+		type: String,
+		required: false
+	},
+    dbc_file_name: {
+		type: String,
+		required: false
+	},
+});
 
-const Project = mongoose.model("Project", mySchema);
+const Project = mongoose.model("Project", projectSchema);
+const Session = mongoose.model("Session", sessionSchema);
 
 module.exports = Project;
+module.exports = Session;
