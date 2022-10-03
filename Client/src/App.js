14b0bf6d58decfe8fdcce9e636e958c62544a3e5
andymunoz <admunoz8@miners.utep.edu>
2022-10-03 10:53:48 -0600

    fix everything

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProjectConfigurationHolder from './project-config/ProjectConfigurationHolder';
-import CanBusConfigurationHolder from './can-bus-config/CanBusConfigurationHolder';
 import SessionConfigurationHolder from './session-config/SessionConfigurationHolder';
 import SplitView from './split-view/SplitView';
 
@@ -10,11 +8,8 @@ function App(){
    return(
 <Router>
     <Routes>
-        <Route exact path="/" element={<ProjectConfigurationHolder />}></Route>
+        <Route exact path="/" element={<SessionConfigurationHolder/>}></Route>
         <Route exact path="/main" element={< SplitView/>}></Route>
-        <Route exact path="/canbus" element={<CanBusConfigurationHolder />}></Route>
-        <Route exact path="/session" element={<SessionConfigurationHolder />}></Route>
-        <Route exact path="/splitview" element={<SplitView />}></Route>
     </Routes>
 </Router>
    );
