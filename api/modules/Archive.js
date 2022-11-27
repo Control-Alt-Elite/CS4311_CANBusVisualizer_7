ff607d013c590aff3ce552a5d2191ef5e4295105
Nicole-favela <nefavela@miners.utep.edu>
2022-11-27 17:13:15 -0500

    works to move files inside src directory into dest archive with overrride set to true

--- api/modules/Archive.js
+++ api/modules/Archive.js
@@ -2,7 +2,9 @@
 //import fs from "fs-extra";
 const fs = require('fs-extra');
   
-// TODO: test this with project folder on system and archive project folder
+// Works: use fullpath, if both are folders copies contents of src folder into dest folder
+// TODO: get to work with fs.move-errors enoent no such file
+//works with moveSync but original files in dest
 const path = require("path");
 
 const Archive = async (src)=> {
@@ -10,15 +12,13 @@ const Archive = async (src)=> {
     // Source file
     //const src} =req.body; 
     console.log("in archive.js: filepath:",src)
-    //console.log("in archive.js ",req.body)
-    //const src = "home/kali/TestProjectArchive/projecttest.txt";
     
     // Destination path
     const dest = "/home/kali/Archives";
     
-    fs.copy(src, dest, (err) => {
+    fs.move(src, dest,{overwrite:true},(err) => {
         if (err) return console.log(err);
-        console.log(`File successfully moved!!`);
+        console.log(`File archived successfully`);
     });
 }
 module.exports = Archive;
