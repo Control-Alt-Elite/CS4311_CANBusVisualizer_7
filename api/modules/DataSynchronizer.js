5a083c3d0a15ca505743cf72f7cd71f8bb99458a
Efrain Retana <eretana1@miners.utep.edu>
2022-11-29 01:50:25 -0500

    add small changes to sync

--- api/modules/DataSynchronizer.js
+++ api/modules/DataSynchronizer.js
@@ -1,20 +1,20 @@
 var Rsync = require('rsync');
 var fs = require('fs');
-const { spawn } = require('child_process');  
+const { exec, spawn } = require('child_process');
 
 const syncPWPath = './sync/sync-data.txt';
 
-function savePasswordToSecret(password){
-  return fs.writeFileSync('./sync/sync-data.txt', `${password}`, {flag: 'w+', mode: 400});
+// Creates secret file with pw for sync
+function savePasswordToSecret(password) {
+  return fs.writeFileSync('./sync/sync-data.txt', `${password}`, { flag: 'w+', mode: 400 });
 }
 
 module.exports = function (sync, req, res) {
   var syncFile = savePasswordToSecret(sync.password);
 
-  var rsyncCommand = `sshpass -p $(cat ./sync/sync-data.txt) rsync -ave ssh ${sync.senderFilePath} ${sync.username}@${sync.receiverIp}:${sync.receiverFilePath}`;
-  // TODO: Update source and destination
-  console.log(`File descriptor of file: ${syncFile}`);
-  var rsyncPid = spawn(rsyncCommand,[], {shell: true});
+  // Rsync command spawned
+  var rsyncCommand = `sshpass -p $(cat ./sync/sync-data.txt) rsync -ave ssh -o StrictHostKeyChecking=no -l root ${sync.senderFilePath} ${sync.username}@${sync.receiverIp}:${sync.receiverFilePath}`;
+  var rsyncPid = spawn(rsyncCommand, [], { shell: true });
 
   // signal handler function
   var quitting = function () {
@@ -40,17 +40,18 @@ module.exports = function (sync, req, res) {
     "Connection": "keep-alive"
   });
 
-  // Read 
+  // Response from rsync command
   rsyncPid.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
     emitSyncResponse(res, data, 'In progress');
   });
-  
+
   rsyncPid.stderr.on('data', (data) => {
     console.error(`stderr: ${data}`);
+    console.log(`stderr: ${data}`)
     emitSyncResponse(res, data, 'Incomplete');
   });
-  
+
   rsyncPid.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
     fs.unlink('./sync/sync-data.txt');
